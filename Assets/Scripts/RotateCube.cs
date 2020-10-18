1e333769385e980d3f5bc73a5f33843d9edee177
Austin-Derrick <aderrickson@colum.edu>
2020-10-18 14:09:46 -0500

    Added simple character controler and a rotate function to rotate the stencilmask cube

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    Vector3 angleToRotate = new Vector3(0, 0.1f, 0);
+
+    private void Update()
+    {
+        transform.Rotate(angleToRotate);
+    }
+}
