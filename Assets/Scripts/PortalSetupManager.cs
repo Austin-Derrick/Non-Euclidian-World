40b3f25d6240e5a935fcbdd89fc9912f202ff202
Austin-Derrick <aderrickson@colum.edu>
2020-10-26 08:43:57 -0500

    Added a custom script to set up a render texture relative to screensize on load

---
+++ Assets/Scripts/PortalSetupManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalSetupManager : MonoBehaviour
+{
+    public Camera cameraRight;
+    public Material cameraMatRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cameraRight.targetTexture != null)
+        {
+            cameraRight.targetTexture.Release();
+        }
+        cameraRight.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatRight.mainTexture = cameraRight.targetTexture;
+    }
+}
