7c090d78e825188f8248676c67f88d408e945475
Austin-Derrick <aderrickson@colum.edu>
2020-10-25 22:53:12 -0500

    Added portal objects to portal scene

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public Transform player;
+	public BoxCollider reciever;
+
+	private bool playerIsOverlapping = false;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!playerIsOverlapping)
+		{
+			Vector3 portalToPlayer = player.position - transform.position;
+			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+			// If this is true: The player has moved across the portal
+			//if (dotProduct < 0f)
+			//{
+				Debug.Log("Teleporting player");
+				// Teleport him!
+				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.transform.rotation);
+				rotationDiff += 180;
+				player.Rotate(Vector3.up, rotationDiff);
+
+				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer + new Vector3(0, 0, 5);
+				player.position = reciever.transform.position + positionOffset;
+
+				playerIsOverlapping = false;
+			//}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = false;
+		}
+	}
+
+}
