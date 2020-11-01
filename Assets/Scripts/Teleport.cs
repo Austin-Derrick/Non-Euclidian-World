6f03c482fd90ac476dc322f541cd464a2889d2a0
Austin-Derrick <aderrickson@colum.edu>
2020-11-01 15:58:54 -0600

    Updated the teleport script in order to face the way to player was walking into the portal and so the player can only enter the portal from the right side.

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -13,25 +13,23 @@ public class Teleport : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (!playerIsOverlapping)
+		if (playerIsOverlapping)
 		{
 			Vector3 portalToPlayer = player.position - transform.position;
 			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-			// If this is true: The player has moved across the portal
-			//if (dotProduct < 0f)
-			//{
-				Debug.Log("Teleporting player");
-				// Teleport him!
+            // If this is true: The player has moved across the portal
+            if (dotProduct < 0f)
+            {
 				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.transform.rotation);
-				rotationDiff += 180;
+				//rotationDiff += 180;
 				player.Rotate(Vector3.up, rotationDiff);
 
-				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer + new Vector3(0, 0, 5);
+				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer; //+ new Vector3(0, 0, 5);
 				player.position = reciever.transform.position + positionOffset;
 
 				playerIsOverlapping = false;
-			//}
-		}
+            }
+        }
 	}
 
 	void OnTriggerEnter(Collider other)
