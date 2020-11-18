81d5769f05cd7bd4a374b75d88dcbbd0f45a4d9e
Austin-Derrick <aderrickson@colum.edu>
2020-11-17 20:32:57 -0600

    Added another field in the script to account for a second portal

--- Assets/Scripts/PortalSetupManager.cs
+++ Assets/Scripts/PortalSetupManager.cs
@@ -6,9 +6,20 @@ public class PortalSetupManager : MonoBehaviour
 {
     public Camera cameraRight;
     public Material cameraMatRight;
+
+    public Camera cameraLeft;
+    public Material cameraMatLeft;
     // Start is called before the first frame update
     void Start()
     {
+
+        if (cameraLeft.targetTexture != null)
+        {
+            cameraLeft.targetTexture.Release();
+        }
+        cameraLeft.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatLeft.mainTexture = cameraLeft.targetTexture;
+
         if (cameraRight.targetTexture != null)
         {
             cameraRight.targetTexture.Release();
