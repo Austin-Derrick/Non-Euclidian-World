e6f289483c0393b238186ef6166d3b5ca047f5b3
Austin-Derrick <aderrickson@colum.edu>
2020-12-13 23:14:13 -0600

    Adjusted screen cutout shader and saved teleport scene

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -7,48 +7,25 @@ public class Teleport : MonoBehaviour
 {
 	// Source code help https://www.youtube.com/watch?v=cuQao3hEKfs
 	public Transform player;
-	public BoxCollider reciever;
+	public Transform reciever;
 
-	private bool playerIsOverlapping = false;
-	int counter = 0;
-	// Update is called once per frame
-	void Update()
-	{
-		if (playerIsOverlapping && counter == 1)
-		{
-			Vector3 portalToPlayer = player.position - transform.position;
-			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-            // If this is true: The player has moved across the portal
-            if (dotProduct < 0f)
-            {
-				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.transform.rotation);
-				rotationDiff += 180;
-				player.Rotate(Vector3.up, rotationDiff);
-
-				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer; //+ new Vector3(0, 0, 5);
-				player.position = reciever.transform.position + positionOffset;
-
-				playerIsOverlapping = false;
-            }
-			counter++;
-		}
-	}
+	public bool negative;
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			playerIsOverlapping = true;
-			counter = 1;
-		}
-	}
+			Vector3 portalToPlayer = player.position - transform.position;
+			float dotProduct = Vector3.Dot(transform.forward, portalToPlayer);
 
-	void OnTriggerExit(Collider other)
-	{
-		if (other.tag == "Player")
-		{
-			playerIsOverlapping = false;
-			counter = 0;
+            if (dotProduct > 0 && negative)
+            {
+				player.position = reciever.position + portalToPlayer;
+            }
+            else if (dotProduct < 0 && !negative)
+            {
+				player.position = reciever.position + portalToPlayer;
+			}
 		}
 	}
 
